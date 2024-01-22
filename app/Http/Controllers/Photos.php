e7d72407810873c55e36a868de6b431e452ce7f9
Itzel2503 <71886544+Itzel2503@users.noreply.github.com>
2024-01-22 13:26:09 -0600

    Update Photos.php

--- app/Http/Controllers/Photos.php
+++ app/Http/Controllers/Photos.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\File;
 
 class Photos extends Controller
 {
@@ -35,13 +37,32 @@ class Photos extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
+    {   
+        // Obtener la imagen en formato base64
+        $base64Image = $request->input('image_file');
+
+        // Decodificar el contenido base64 y guardarlo en un archivo temporal
+        $imageData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64Image));
+        $tempPath = tempnam(sys_get_temp_dir(), 'img');
+        file_put_contents($tempPath, $imageData);
+
+        // Obtener la información necesaria del formulario
+        $namePhoto = $request->input('name_photo');
+        $destinationPath = $request->input('destination_path');
+
+        // Crear una instancia de File para el archivo temporal
+        $imageFile = new File($tempPath);
+
+        // Guardar el archivo en el almacenamiento local
+        Storage::disk('local')->put('/' . $namePhoto . '.jpg', file_get_contents($tempPath));
+
+
         DB::table('photos')->insert([
             'name' => $request->name_photo,
             'created_at' => Carbon::now(),
         ]);
 
-        return redirect('/');
+        return redirect('/camera');
     }
 
     /**
